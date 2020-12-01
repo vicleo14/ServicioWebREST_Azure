faeea03187c80f7d45aa7b5780cf8ff359ac9a60
Victor <vicleo.morales@hotmail.com>
2020-12-01 01:24:30 -0600

    Inicio

---
+++ conf.sh
@@ -0,0 +1,49 @@
+
+#!/bin/bash
+
+
+# Extraccion de Tomcat
+unzip apache-tomcat-8.5.60.zip
+rm -r apache-tomcat-8.5.60/webapps
+mkdir apache-tomcat-8.5.60/webapps
+mkdir apache-tomcat-8.5.60/webapps/ROOT
+
+
+# Extraccion de Jersey 
+unzip jaxrs-ri-2.24.zip
+
+# Copia de archivos jar de Jersey a Tomcat
+cp jaxrs-ri/api/*.jar apache-tomcat-8.5.60/lib/
+cp jaxrs-ri/ext/*.jar apache-tomcat-8.5.60/lib/
+cp jaxrs-ri/lib/*.jar apache-tomcat-8.5.60/lib/
+
+#Solucion de error de compatibilidad de Tomcat con Jersey. El jar proviene de Jersey
+rm apache-tomcat-8.5.60/lib/javax.servlet-api-3.0.1.jar
+
+# Copia GSON a Tomcat
+cp gson-2.3.1.jar apache-tomcat-8.5.60/lib/
+
+#Driver JDBC de MySQL
+unzip mysql-connector-java-8.0.22.zip
+cp  mysql-connector-java-8.0.22/mysql-connector-java-8.0.22.jar apache-tomcat-8.5.60/lib/
+
+# Actualizacion de APT
+sudo apt update
+# Instalacion de JDK 8
+sudo apt install openjdk-8-jdk-headless
+
+##### Instalacion de MySQL
+sudo apt install mysql-server
+
+# Ejecucion de script de seguridad MySQL
+sudo mysql_secure_installation
+
+
+##### Prueba del servicio web
+
+cp usuario_sin_foto.png apache-tomcat-8.5.60/webapps/
+cp prueba.html apache-tomcat-8.5.60/webapps/
+
+
+cp WSClient.js apache-tomcat-8.5.60/webapps/
+
